d180099abab59c2a1ebce555c189a0050b3fa6d4
Cheng Luo <luochengleo@gmail.com>
2016-03-23 09:41:47 +0900

    make a better selenium crawler

--- src/GetResultHref.py
+++ src/GetResultHref.py
@@ -12,6 +12,7 @@ def extractUrls():
     items = cursor.fetchall()
     fout = open('../data/url.tsv','w')
 
+    alreadyin = set()
     for item in items:
         globalid = str(item[0])
         q = item[1]
@@ -21,9 +22,36 @@ def extractUrls():
         soup = BeautifulSoup(content)
         url= soup.find('a').get('href')
         if rank <= 5:
-            print globalid
             fout.write(globalid+'\t'+str(rank)+'\t'+q+'\t'+url+'\n')
+            alreadyin.add(globalid)
+
+
+
+    local2global = dict()
+    for l in open('../data/allsearchresult.tsv'):
+        globalid,query,localid = l.strip().split('\t')
+        local2global[(query,localid)] = globalid
+
+
+    cursor.execute("SELECT * FROM anno_annotation")
+    items = cursor.fetchall()
+    for item in items:
+        query = str(item[3])
+        localid = str(item[4])
+        try:
+            globalid = local2global[(query,localid)]
+            rank = 0
+            url = item[5]
+            if globalid in alreadyin:
+                print 'already in !', globalid
+            else:
+                alreadyin.add(globalid)
+                fout.write(globalid+'\t'+str(rank)+'\t'+query+'\t'+url+'\n')
+        except:
+            print query,localid
+
 
     fout.close()
+    cursor.close()
 
 extractUrls()
--- src/PageCrawler.py
+++ src/PageCrawler.py
@@ -3,46 +3,52 @@ __author__ = 'luocheng'
 pages = []
 import os
 import sys
+import random
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 from selenium import webdriver
 
 
 while True:
-    finished = os.listdir('../data/pages')
-    for l in open('../data/url.tsv'):
-        globalid, _, _, url = l.strip().split('\t')
-        if globalid + '.html' not in finished:
-            pages.append((globalid, url))
-        else:
-            print 'SKIP', globalid
+    try:
+        finished = os.listdir('../data/pages')
+        for l in open('../data/url.tsv'):
+            globalid, _, _, url = l.strip().split('\t')
+            if globalid + '.html' not in finished:
+                pages.append((globalid, url))
+            else:
+                print 'SKIP', globalid
+        random.shuffle(pages)
+        for item in pages:
+            globalid = item[0]
+            url = item[1]
+            browser = webdriver.Firefox()
+            browser.set_script_timeout(30)
+            browser.set_page_load_timeout(30)
+            browser.implicitly_wait(30)
+            try:
+                browser.get(url)
+                file = open('../data/pages/' + globalid + '.html', 'w')
+                file.write(browser.page_source)
+                file.close()
+                browser.close()
+                print 'Success', globalid
+            except:
+                import sys
+                print  sys.exc_info()[0]
+                print 'Failure', globalid, url
+            finally:
+                file = open('../data/pages/' + globalid + '.html', 'w')
+                file.write(browser.page_source)
+                file.close()
+                browser.close()
 
-    for item in pages:
-        globalid = item[0]
-        url = item[1]
-        browser = webdriver.Firefox()
-        browser.set_script_timeout(30)
-        browser.set_page_load_timeout(30)
-        browser.implicitly_wait(30)
-        try:
+        finished = os.listdir('../data/pages')
+        for item in finished:
+            size = os.path.getsize('../data/pages/' + item)
+            if size < 10 * 1024:
+                os.system("rm ../data/pages/" + item)
+                print 'REMOVE ' + item
 
-            browser.get(url)
-            file = open('../data/pages/' + globalid + '.html', 'w')
-            file.write(browser.page_source)
-            file.close()
-            browser.close()
-
-            print 'Success', globalid
-        except:
-            import sys
-            print  sys.exc_info()[0]
-            print 'Failure', globalid, url
-        finally:
-            browser.close()
-    finished = os.listdir('../data/pages')
-    for item in finished:
-        size = os.path.getsize('../data/pages/' + item)
-        if size < 10 * 1024:
-            os.system("rm ../data/pages/" + item)
-            print 'REMOVE ' + item
-
+    except:
+        print 'BEGIN Again!'
